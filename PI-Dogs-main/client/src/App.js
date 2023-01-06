9d368b09850403e73090993248979990ee6ae0f4
Josecanelo <yojose_6@hotmail.com>
2023-01-05 21:42:58 -0300

    deploy-back-final

--- PI-Dogs-main/client/src/App.js
+++ PI-Dogs-main/client/src/App.js
@@ -5,7 +5,7 @@ import Landing from "./components/Landing/Landing"
 import Details from "./components/Details/Details"
 import { Route } from 'react-router-dom';
 import axios from 'axios';
-axios.defaults.baseURL = "http://localhost:3001"
+axios.defaults.baseURL = "pi-dogs-production-eddc.up.railway.app"
  
 
 function App() {
